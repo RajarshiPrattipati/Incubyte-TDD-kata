13e66c0ee0e93994eb461a58d20725d01799d808
Rajarshi P <rishiprattipati@gmail.com>
2024-12-13 18:41:52 +0530

    First test done

--- src/Add.tsx
+++ src/Add.tsx
@@ -1,5 +1,20 @@
-const add = (numbers: String): number => {
-  return 0;
+/* Adds elements in a string, allowed characters are : 
+    1,2,3, .... 1000 are variables
+    `\n` & `'` are delimiters
+
+    Supports custom delimiters. eg, //;\n1;2
+*/
+const add = (numbers: String = ""): number => {
+  console.log("Numbers: ", numbers);
+  let temp = "";
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    console.log("numbers[i]: ", numbers[i]);
+    if (numbers[i] === "\n") {
+    } else temp = temp.concat(numbers[i]);
+  }
+  console.log("TEMP", temp);
+  return temp === "" ? sum : sum + parseInt(temp);
 };
 
 export default add;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,13 @@
 import add from "./Add";
 
-test("renders learn react link", () => {
+test("empty string", () => {
   expect(add()).toBe(0);
+});
+
+test("1", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("2", () => {
+  expect(add("2")).toBe(2);
 });
