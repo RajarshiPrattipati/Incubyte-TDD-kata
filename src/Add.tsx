6a2fec29d6c2daf258ff1b30a0c84b332dc86d53
Rajarshi P <rishiprattipati@gmail.com>
2024-12-13 18:53:36 +0530

    Third test done

--- src/Add.tsx
+++ src/Add.tsx
@@ -5,20 +5,17 @@
     Supports custom delimiters. eg, //;\n1;2
 */
 const add = (numbers: String = ""): number => {
-  console.log("Numbers: ", numbers);
   let temp = "";
   let delimiter = ",";
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
-    console.log("numbers[i]: ", numbers[i]);
-    if (numbers[i] === delimiter) {
+    // console.log("numbers[i]: ", numbers[i]);
+    if ([delimiter, "\n"].includes(numbers[i])) {
       if (parseInt(temp)) sum += parseInt(temp);
       else throw new Error("No value present before delimiter");
       temp = "";
-    } else if (numbers[i] === "\n") {
     } else temp = temp.concat(numbers[i]);
   }
-  console.log("TEMP", temp);
   return temp === "" ? sum : sum + parseInt(temp);
 };
 
