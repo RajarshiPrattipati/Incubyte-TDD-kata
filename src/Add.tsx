331f15d10c357060882a01c27b2acd2207803861
Rajarshi P <rishiprattipati@gmail.com>
2024-12-13 18:46:47 +0530

    Second test done

--- src/Add.tsx
+++ src/Add.tsx
@@ -7,10 +7,15 @@
 const add = (numbers: String = ""): number => {
   console.log("Numbers: ", numbers);
   let temp = "";
+  let delimiter = ",";
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
     console.log("numbers[i]: ", numbers[i]);
-    if (numbers[i] === "\n") {
+    if (numbers[i] === delimiter) {
+      if (parseInt(temp)) sum += parseInt(temp);
+      else throw new Error("No value present before delimiter");
+      temp = "";
+    } else if (numbers[i] === "\n") {
     } else temp = temp.concat(numbers[i]);
   }
   console.log("TEMP", temp);
